Corrected an error in bow_training == True behaviour of BowNet. NormalizedLinearLayer should have been receiving the GlobalAvgPooled vector from resblock3_256b_fmaps. Fixed the softmax shapes so that we don't have to do that '[:, 0]' indexing any more. Updated all relevant scripts

model.py
@@ -18,6 +18,7 @@ class BowNet(nn.Module):
         """Define layers"""
         super().__init__()
         self.num_classes = num_classes
+        self.bow_training = bow_training
         self.conv1_64 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1_64 = nn.BatchNorm2d(num_features=64)
         self.relu1_64 = nn.ReLU()
@@ -32,10 +33,12 @@ class BowNet(nn.Module):
         self.resblock4_512b = ResidualBlock(in_channels=512, out_channels=512, kernel_size=3, downsample_factor=1, use_dropout=True, dropout_rate=0.5)
 
         self.global_avg_pool = nn.AvgPool2d(kernel_size=8, stride=1)
-        if bow_training:
-            self.fc_out = NormalizedLinear(512, self.num_classes)
+        if self.bow_training:
+            self.fc_out = NormalizedLinear(256, self.num_classes)
+            self.fc_fin = 256
         else:
             self.fc_out = nn.Linear(512, self.num_classes)
+            self.fc_fin = 512
         # self.fc_out = nn.Linear(512,1)
         self.initialize()
 
@@ -59,10 +62,12 @@ class BowNet(nn.Module):
         # We will need these feature maps for the K-means clustering to create a Visual BoW vocabulary
         self.resblock3_256b_fmaps = x
 
-        x = self.resblock4_512a(x)
-        x = self.resblock4_512b(x)
+        if not self.bow_training:
+            # Only add these residual layers for the Rotation pre-training
+            x = self.resblock4_512a(x)
+            x = self.resblock4_512b(x)
 
-        x = self.global_avg_pool(x).reshape(-1, 1, 512)
+        x = self.global_avg_pool(x).reshape(-1, self.fc_fin)
         x = self.fc_out(x)
         logits = x
         # print("bownet x shape", x.shape)
@@ -141,7 +146,7 @@ class BowNet2(nn.Module):
         x = self.resblock4_512c(x)
         x = self.resblock4_512d(x)
 
-        x = self.global_avg_pool(x).reshape(-1, 1, 512)
+        x = self.global_avg_pool(x).reshape(-1, 512)
         x = self.fc_out(x)
         logits = x
         # print("bownet x shape", x.shape)
@@ -226,7 +231,7 @@ class BowNet3(nn.Module):
         x = self.resblock4_512b(x)
         x = self.resblock4_512c(x)
 
-        x = self.global_avg_pool(x).reshape(-1, 1, 512)
+        x = self.global_avg_pool(x).reshape(-1, 512)
         x = self.fc_out(x)
         logits = x
         # print("bownet x shape", x.shape)
@@ -262,7 +267,7 @@ class LinearClassifier(nn.Module):
         #self.fc1 = nn.Linear(6400,self.num_classes)
         self.fc2 = nn.Linear(1000,self.num_classes)
 
-        self.initilize()
+        self.initialize()
 
 
     def forward(self, input_tensor):
@@ -273,22 +278,23 @@ class LinearClassifier(nn.Module):
 
         # x = F.relu(x)
         x = self.fc1(x)
-        x = x.reshape(-1, 1, self.num_classes)
+        x = x
 
         logits = x
         preds = F.softmax(x, dim=-1)
 
         return logits, preds
 
-    def initilize(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                fin = m.in_features
-                fout = m.out_features
+    def initialize(self):
+        for m in self._modules:
+            block = self._modules[m]
+            if isinstance(block, nn.Linear):
+                fin = block.in_features
+                fout = block.out_features
                 std_val = np.sqrt(2.0 / fout)
-                m.weight.data.normal_(0.0, std_val)
-                if m.bias is not None:
-                    m.bias.data.fill_(0.0)
+                block.weight.data.normal_(0.0, std_val)
+                if block.bias is not None:
+                    block.bias.data.fill_(0.0)
 
 class NonLinearClassifier(nn.Module):
 
@@ -325,7 +331,7 @@ class NonLinearClassifier(nn.Module):
 
         x = self.fc_out(x)
 
-        x = x.reshape(-1, 1, self.num_classes)
+        x = x
         logits = x
         preds = F.softmax(x, dim=-1)
 
@@ -342,8 +348,8 @@ class NonLinearClassifier(nn.Module):
                     m.bias.data.fill_(0.0)
 
 
-def load_checkpoint(checkpoint,device, bownet_arch):
-    bownet = bownet_arch(num_classes=4).to(device)
+def load_checkpoint(checkpoint, device, bownet_arch, num_classes=4, bow_training=False):
+    bownet = bownet_arch(num_classes, bow_training).to(device)
     optimizer = optim.SGD(bownet.parameters(), lr=0.1, momentum=0.9,weight_decay= 5e-4)
 
     bownet.load_state_dict(checkpoint['model_state_dict'])

rotnet_train_test.py
@@ -86,11 +86,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 num_epochs = 100
 bownet = BowNet(num_classes=4).to(device)
 criterion = nn.CrossEntropyLoss().to(device)
-#optimizer = optim.SGD(bownet.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4) # used for BowNet2 and 3
-#optimizer = optim.Adam(bownet.parameters(), lr=0.01)
 optimizer = optim.SGD(bownet.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-#lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.2)
-#lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.2)
 lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.2, patience=10)
 
 # tensors = {}
@@ -130,10 +126,9 @@ with torch.cuda.device(0):
             # forward + backward + optimize
             logits, preds = bownet(inputs)
 
-            # print(preds[:,0])
 
             #Compute loss
-            loss = criterion(logits[:, 0], labels)
+            loss = criterion(logits, labels)
 
             #Back Prop and Optimize
             loss.backward()
@@ -143,7 +138,7 @@ with torch.cuda.device(0):
             running_loss += loss.item()
 
             loss_100 += loss.item()
-            acc_batch, batch_correct_preds = accuracy(preds[:,0].data, labels, topk=(1,))
+            acc_batch, batch_correct_preds = accuracy(preds.data, labels, topk=(1,))
             accs.append(acc_batch[0].item())
             total_correct += batch_correct_preds
             total_samples += preds.size(0) 
@@ -204,13 +199,13 @@ with torch.cuda.device(0):
             # print(preds[:,0])
 
             #Compute loss
-            loss = criterion(logits[:,0], labels)
+            loss = criterion(logits, labels)
 
 
             # print statistics
             running_loss += loss.item()
 
-            acc_batch, batch_correct_preds = accuracy(preds[:,0].data, labels, topk=(1,))
+            acc_batch, batch_correct_preds = accuracy(preds.data, labels, topk=(1,))
             accs.append(acc_batch[0].item())
             test_correct += batch_correct_preds
             test_total += preds.size(0) 