5a940faa5251f212e3d7258e3ca303f9861b4697
StoneY1 <stone.yun@mail.mcgill.ca>
2020-12-04 02:33:22 +0000

    cleaned up redundant code and now calling from the proper helper functions

--- rotnet_linearclf.py
+++ rotnet_linearclf.py
@@ -2,13 +2,15 @@ from __future__ import print_function
 import argparse
 import os
 import imp
-from dataloader import DataLoader, GenericDataset
+from dataloader import DataLoader, GenericDataset, get_dataloader
 import matplotlib.pyplot as plt
 
 
 import copy
-from model import BowNet, load_checkpoint, LinearClassifier, NonLinearClassifier
-#from model import BowNet3 as BowNet
+from model import LinearClassifier, NonLinearClassifier
+from utils import load_checkpoint, accuracy
+from model import BowNet
+#from model import BowNet2 as BowNet
 from tqdm import tqdm
 import torch
 import torch.nn as nn
@@ -25,81 +27,17 @@ from sklearn.cluster import MiniBatchKMeans
 #from kmeans_pytorch import kmeans
 
 # Set train and test datasets and the corresponding data loaders
-batch_size = 128
+batch_size = 64
 
-data_train_opt = {}
-data_train_opt['batch_size'] = batch_size
-data_train_opt['unsupervised'] = False #Set to False then the label is the class - Set to True to get rotation label
-data_train_opt['epoch_size'] = None
-data_train_opt['random_sized_crop'] = False
-data_train_opt['dataset_name'] = 'cifar100'
-data_train_opt['split'] = 'train'
-
-data_test_opt = {}
-data_test_opt['batch_size'] = batch_size
-data_test_opt['unsupervised'] = False #Set to False then the label is the class - Set to True to get rotation label
-data_test_opt['epoch_size'] = None
-data_test_opt['random_sized_crop'] = False
-data_test_opt['dataset_name'] = 'cifar100'
-data_test_opt['split'] = 'test'
-
-imgs_per_cat = data_train_opt['imgs_per_cat'] if ('imgs_per_cat' in data_train_opt) else None
-
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        correct_preds = copy.deepcopy(correct_k)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res, correct_preds.int().item()
-
-
-
-dataset_train = GenericDataset(
-    dataset_name=data_train_opt['dataset_name'],
-    split=data_train_opt['split'],
-    random_sized_crop=data_train_opt['random_sized_crop'],
-    num_imgs_per_cat=imgs_per_cat)
-dataset_test = GenericDataset(
-    dataset_name=data_test_opt['dataset_name'],
-    split=data_test_opt['split'],
-    random_sized_crop=data_test_opt['random_sized_crop'])
-
-dloader_train = DataLoader(
-    dataset=dataset_train,
-    batch_size=data_train_opt['batch_size'],
-    mode = 'cifar',
-    unsupervised=data_train_opt['unsupervised'],
-    epoch_size=data_train_opt['epoch_size'],
-    num_workers=4,
-    shuffle=True)
-
-dloader_test = DataLoader(
-    dataset=dataset_test,
-    batch_size=data_test_opt['batch_size'],
-    unsupervised=data_test_opt['unsupervised'],
-    mode = 'cifar',
-    epoch_size=data_test_opt['epoch_size'],
-    num_workers=4,
-    shuffle=False)
-
+dloader_train = get_dataloader('train', 'cifar', batch_size)
+dloader_test = get_dataloader('test', 'cifar', batch_size)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
 
-PATH = "best_bownet_checkpoint1_7285acc.pt"
-checkpoint = torch.load(PATH)
+PATH = "rotnet2_checkpoint.pt"
+#PATH = "best_bownet_checkpoint1_7285acc.pt"
 
-bownet,_,_,_ = load_checkpoint(checkpoint,device,BowNet)
+rotnet, _, _, _ = load_checkpoint(PATH, device, BowNet)
 
 classifier = LinearClassifier(100).to(device)
 #classifier = LinearClassifier(100, 256, 8).to(device)
@@ -110,10 +48,10 @@ optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9, weight_de
 #optimizer = optim.SGD(classifier.parameters(), lr=0.1, momentum=0.9, weight_decay=0.001)
 lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.2)
 
-for para in bownet.parameters():
+for para in rotnet.parameters():
     para.requires_grad = False
 
-bownet.eval()
+rotnet.eval()
 with torch.cuda.device(0):
 
     classifier.train()
@@ -129,32 +67,24 @@ with torch.cuda.device(0):
         accs = []
         total_correct = 0
         total_samples = 0
-        # Need to set bownet to evaluate so that it uses frozen BatchNorm params and no Dropout
-        bownet.eval()
+        # Need to set rotnet to evaluate so that it uses frozen BatchNorm params and no Dropout
+        rotnet.eval()
         classifier.train()
         for idx, batch in enumerate(tqdm(dloader_train(epoch))): #We feed epoch in dloader_train to get a deterministic batch
-        # for idx, batch in enumerate(dloader_train(epoch)):
 
             start_time = time.time()
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = batch
 
-            # check_input = inputs[0].permute(1, 2, 0)
-
 
             #Load data to GPU
             inputs, labels = inputs.cuda(), labels.cuda()
 
-            bownet(inputs)
-            conv_out = bownet.resblock3_256b_fmaps
-
-            # print(conv_out.shape)
+            rotnet(inputs)
+            conv_out = rotnet.resblock4_512_fmaps
 
 
             time_load_data = time.time() - start_time
-
-            # print(labels.shape)
-
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -162,16 +92,12 @@ with torch.cuda.device(0):
             # forward + backward + optimize
             logits, preds = classifier(conv_out)
 
-            # print(preds[:,0])
-
             #Compute loss
             loss = criterion(logits, labels)
 
             #Back Prop and Optimize
             loss.backward()
             optimizer.step()
-
-            # print(classifier.fc1_out.weight.grad)
 
             # print statistics
             running_loss += loss.item()
@@ -183,15 +109,7 @@ with torch.cuda.device(0):
             total_correct += batch_correct_preds
             total_samples += preds.size(0)
 
-            # if idx % 100 == 99:
-            #     print('[%d, %5d] loss: %.3f' %
-            #           (epoch , idx, loss_100/100))
-            #     loss_100 = 0.0
-            #     acc = accuracy(logits[:,0], labels, topk=(1,))[0].item()
-            #     print("accuracy after 100 batch: ",acc)
-            #     print("Time to finish 100 batch", time.time() - start_time)
 
-        accs = np.array(accs)
         print("epoch training accuracy: ", 100*total_correct/total_samples)
 
         print("Time to load the data", time_load_data)
@@ -222,16 +140,14 @@ with torch.cuda.device(0):
             time_load_data = time.time() - start_time
 
 
-            # forward + backward + optimize
-            bownet(inputs)
-            conv_out = bownet.resblock3_256b_fmaps
+            rotnet(inputs)
+            conv_out = rotnet.resblock4_512_fmaps
 
             logits, preds = classifier(conv_out)
 
 
             #Compute loss
             loss = criterion(logits, labels)
-
 
             # print statistics
             running_loss += loss.item()
@@ -241,8 +157,8 @@ with torch.cuda.device(0):
             test_correct += batch_correct_preds
             test_total += preds.size(0)
 
-
-        lr_scheduler.step() # Use this if not using ReduceLROnPlateau scheduler
+        #lr_scheduler.step() # Use this if not using ReduceLROnPlateau scheduler
+        lr_scheduler.step(running_loss/len(dloader_test))
         accs = np.array(accs)
         #print("epoche test accuracy: ",accs.mean())
         print("epoch test accuracy: ", 100*test_correct/test_total)
--- test_model.py
+++ test_model.py
@@ -6,11 +6,11 @@ import argparse
 import copy
 import os
 import imp
-from dataloader import DataLoader, GenericDataset
+from dataloader import DataLoader, GenericDataset, get_dataloader
 import matplotlib.pyplot as plt
 
 from model import BowNet
-from model import load_checkpoint
+from utils import load_checkpoint, accuracy
 from tqdm import tqdm
 import torch
 import torch.nn as nn
@@ -22,56 +22,16 @@ import numpy as np
 # Set train and test datasets and the corresponding data loaders
 batch_size = 128
 
-data_test_opt = {}
-data_test_opt['batch_size'] = batch_size
-data_test_opt['unsupervised'] = True
-data_test_opt['epoch_size'] = None
-data_test_opt['random_sized_crop'] = False
-data_test_opt['dataset_name'] = 'cifar100'
-data_test_opt['split'] = 'test'
-
-imgs_per_cat = None
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        correct_preds = copy.deepcopy(correct_k)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res, correct_preds.int().item()
-
-dataset_test = GenericDataset(
-    dataset_name=data_test_opt['dataset_name'],
-    split=data_test_opt['split'],
-    random_sized_crop=data_test_opt['random_sized_crop'])
-
-dloader_test = DataLoader(
-    dataset=dataset_test,
-    batch_size=data_test_opt['batch_size'],
-    unsupervised=data_test_opt['unsupervised'],
-    epoch_size=data_test_opt['epoch_size'],
-    num_workers=4,
-    shuffle=False)
+dloader_test = get_dloader('test', 'cifar', batch_size)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#device = torch.device('cpu')
 num_epochs = 200
 bownet = BowNet(num_classes=4).to(device)
 criterion = nn.CrossEntropyLoss().to(device)
 
-
 PATH = "best_rotnet_checkpoint1_7985acc.pt"
-checkpoint = torch.load(PATH, map_location=torch.device('cpu'))
 
-bownet,_,_,_ = load_checkpoint(checkpoint,device, BowNet)
+bownet,_,_,_ = load_checkpoint(PATH, device, BowNet)
 bownet.eval()
 with torch.cuda.device(0):
     print(f"EVALUATION")
@@ -96,8 +56,6 @@ with torch.cuda.device(0):
         # forward + backward + optimize
         logits, preds = bownet(inputs)
 
-        # print(preds[:,0])
-
         #Compute loss
         loss = criterion(logits, labels)
 
@@ -109,11 +67,8 @@ with torch.cuda.device(0):
         accs.append(acc_batch[0].item())
         test_correct += batch_correct_preds
         test_total += preds.size(0) 
-        #accs.append(accuracy(preds[:,0].data, labels, topk=(1,))[0].item())
 
 
-    # plt.imshow(check_input)
-    # plt.savefig("imag" + str(epoch) + ".png")
     accs = np.array(accs)
     #print("epoche test accuracy: ",accs.mean())
     print("epoch test accuracy: ", 100*test_correct/test_total)
